ab52f788336b12f5e58443e7ffd94ed8b032df78
michaelmaur <michael.maur@outlook.com>
2017-04-18 12:08:25 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStrAmount = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(93, 119);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(266, 23);
+            this.txtName.TabIndex = 3;
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(93, 150);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(130, 23);
+            this.txtAmount.TabIndex = 4;
+            this.txtAmount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAmount_KeyPress);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(93, 86);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(241, 23);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Date:";
+            // 
+            // txtStrAmount
+            // 
+            this.txtStrAmount.Location = new System.Drawing.Point(93, 189);
+            this.txtStrAmount.Name = "txtStrAmount";
+            this.txtStrAmount.Size = new System.Drawing.Size(687, 23);
+            this.txtStrAmount.TabIndex = 13;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.Silver;
+            this.btnPrint.Location = new System.Drawing.Point(93, 259);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(94, 42);
+            this.btnPrint.TabIndex = 14;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Silver;
+            this.btnClear.Location = new System.Drawing.Point(203, 259);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 42);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 329);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.txtStrAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.txtName);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Cheque Writer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStrAmount;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* 
+Add Trapping 
+Input the centavo in 1 blank
+Confirm the process for centavos
+Setup printer task!
+*/
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void menuClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtAmount.Clear();
+            txtStrAmount.Clear();
+
+        }
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                //condition
+
+                if (String.IsNullOrEmpty(txtName.Text))
+                {
+                    MessageBox.Show("Please enter the Name the box Provided", "Empty Field", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtName.Focus();
+                }
+                //CHECK FOR SPECIAL CHARACTERS
+                else if (checkSpecialChar(txtName.Text))
+                {
+                    MessageBox.Show("Please input without any special characters", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtName.Focus();
+                }
+                else if (checkNumber(txtName.Text))
+                {
+                    MessageBox.Show("Please do not include numbers in the name", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtName.Focus();
+                }
+                else
+                    txtAmount.Focus();
+
+            }
+        }
+
+        public Boolean checkSpecialChar(string check)
+        {
+
+            Boolean flag = false;
+
+            // convert each character of the string value in the textbox to ASCII
+            byte[] ASCII = Encoding.ASCII.GetBytes(check);
+            //foreach loop to check each value 
+            foreach (byte b in ASCII)
+            {
+                //condition to check and filter for special characters entered
+                if (b < 31 || (b > 32 && b < 48) || (b > 75 && b < 65) || (b > 90 && b < 97) || b > 122)
+                    flag = true;
+            }
+
+            return flag;
+        }
+
+        public Boolean checkNumber(string check)
+        {
+
+            Boolean flag = false;
+
+            // convert each character of the string value in the textbox to ASCII
+            byte[] ASCII = Encoding.ASCII.GetBytes(check);
+            //foreach loop to check each value 
+            foreach (byte b in ASCII)
+            {
+                //condition to check and filter for special characters entered
+                if (b > 48 && b < 58)
+                    flag = true;
+            }
+
+            return flag;
+        }
+
+        private void txtAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+
+                if (!isNumeric(txtAmount.Text))
+                {
+                    MessageBox.Show("Please input a numeric value", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtAmount.Focus();
+                    txtAmount.Clear();
+                }
+                //condition
+                
+                else
+                {
+
+                    double temp = double.Parse(txtAmount.Text);
+
+
+                    if (temp > 100000000 || temp < 0)
+                    {
+                        MessageBox.Show("Invalid amount", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        txtAmount.Clear();
+                        txtAmount.Focus();
+                    }
+                    else
+                    {
+                        string amount = "";
+                        amount = NumberToWords(Convert.ToInt32(Math.Truncate(temp)));
+                        int result = (int)((temp - (int)temp) * 100);
+                        txtStrAmount.Text = amount + " and " + result + "/100" + " pesos only";
+                    }
+                }
+               
+            }
+
+        }
+        private Boolean isNumeric(string text)
+        {
+            Boolean flag = false;
+            double num;
+
+            flag =  Double.TryParse(text, out num);
+
+            return flag;
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                var tensMap = new[] { "zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+
+            return words;
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            PrintDialog printDlg = new PrintDialog();
+            PrintDocument printDoc = new PrintDocument();
+            printDoc.DocumentName = "Print Document";
+            printDlg.Document = printDoc;
+            printDlg.AllowSelection = true;
+            printDlg.AllowSomePages = true;
+            //Call ShowDialog
+            if (printDlg.ShowDialog() == DialogResult.OK)
+                printDoc.Print();
+
+
+        }
+
+    }
+}
